58441bf0c57b5aa37a1370e35c9c6d7f5fbd421d
shawon1fb <shawon0187@gmail.com>
2022-06-26 23:05:29 +0600

    tower of hanoi

---
+++ recursion/tower_of_hanoi.cpp
@@ -0,0 +1,32 @@
+//
+// Created by shahanul on 26/6/22.
+//
+
+
+#include<bits/stdc++.h>
+
+
+
+
+/// problem  simple
+/// let A B C is three rod
+/// i have to go to rod c
+/// so i will go to rod b first A-> B with help C
+/// then go to c  B-> C with help A
+
+
+using namespace std;
+
+
+void towerOfHanoi(int n, char A, char B, char C) {
+    if (n == 0) return;
+    towerOfHanoi(n - 1, A, C, B);
+    cout << A << " -> " << C << endl;
+    towerOfHanoi(n - 1, B, A, C);
+}
+
+int main() {
+    int n = 2;
+    towerOfHanoi(n, 'A', 'B', 'C');
+    return 0;
+}
